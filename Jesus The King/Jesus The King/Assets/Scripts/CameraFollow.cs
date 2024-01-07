e3a9b6fe4c9291d3a756208e29d70a48f872a12d
Princ <pavol.kralovic26@gmail.com>
2024-01-07 17:27:44 +0100

    Grid + sprites synchronized
    Created first map
    
    Adjusted sizes of player and enemies + bullets and PSs

--- Jesus The King/Jesus The King/Assets/Scripts/CameraFollow.cs
+++ Jesus The King/Jesus The King/Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private Vector3 offset = new Vector3(0f, 0f, -5f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
